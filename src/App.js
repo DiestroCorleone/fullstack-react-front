16efd3c334d359aa639de4e209d5ccc65e4002e9
DiestroCorleone <matias.perez.mcd@gmail.com>
2022-11-21 15:04:41 -0300

    Basic UI, routing, and database query created.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import CreateBlog from './components/CreateBlog';
+import EditBlog from './components/EditBlog';
+import ShowBlogs from './components/ShowBlogs';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Container fluid>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ShowBlogs />} />
+          <Route path="/edit" element={<EditBlog />} />
+          <Route path="/create" element={<CreateBlog />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
   );
 }
--- src/components/ShowBlogs.js
+++ src/components/ShowBlogs.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Table from 'react-bootstrap/Table';
 
 export default function ShowBlogs() {
-  return(
-    <h1>I'm ShowBlogs</h1>
-  )
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const getBlogs = async () => {
+      try {
+        const res = await axios.get(
+          'https://jsonplaceholder.typicode.com/posts'
+        );
+        setBlogs(res);
+      } catch (e) {
+        alert('Error getting blogs: ' + e);
+      }
+    };
+    getBlogs();
+  }, [blogs]);
+
+  const renderBlogs = blogs.map((blog) => {
+    return (
+      <tr>
+        <td>{blog.id}</td>
+        <td>{blog.title}</td>
+        <td>{blog.body}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Content</th>
+      </thead>
+      <tbody>{renderBlogs}</tbody>
+    </Table>
+  );
 }
